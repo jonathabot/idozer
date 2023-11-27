7e0ba0c12b2d443dbe8c7fc4d7e8fe1036ca9f43
jonathabot <lebotelhon@gmail.com>
2023-11-27 20:13:59 -0300

    started project structure | add fonts

--- App.js
+++ App.js
@@ -1,7 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "@use-expo/font";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+
+  const customFonts = {
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+  };
+  let [fontsLoaded] = useFonts(customFonts);
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  const theme = useMemo(() => ({
+    background: {
+      primary: "#EFFAFF",
+      secondary: "#05B494",
+      third: "#D8FFF8",
+      white: "#fff",
+      grey: "##EBEBEB",
+      lightGrey: "rgba(216, 216, 216, 0.2)",
+      orange: "#FFE190"
+
+    },
+    text: {
+      dark: "#222222",
+      light: "#96A5BA",
+      green: "#05B494",
+    },
+  }), []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -17,4 +50,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});4
