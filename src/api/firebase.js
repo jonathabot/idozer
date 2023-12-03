52273e3ce22588d4db240003dc500921ce2ff1bf
Guilherme Peres <drakoocontact@gmail.com>
2023-12-03 14:56:11 -0300

    add firebase

---
+++ src/api/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase'
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDGxvJFk5jV0xxSoMmIJTbrYNptYxAVrxk',
+  authDomain: 'idozer-4246a.firebaseapp.com',
+  databaseURL: 'https://idozer-4246a-default-rtdb.firebaseio.com',
+  projectId: 'idozer-4246a',
+  storageBucket: 'idozer-4246a.appspot.com',
+  messagingSenderId: '236050345521',
+  appId: '1:236050345521:web:e290a37fd955f20872ef10'
+}
+
+const app = initializeApp(firebaseConfig)
