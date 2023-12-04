8f678360760ad037c5ca9675a4841f857726a352
jonathabot <lebotelhon@gmail.com>
2023-12-04 19:24:22 -0300

    simple register page

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, TextInput, Button} from 'react-native';
 
 const RegisterScreen = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function registerUser() {
+    console.log(username);
+    console.log(email);
+    console.log(password);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Tela de Cadastro</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        placeholder={"Username"}
+        onChangeText={(text) => setUsername(text)}
+        autoCapitalize={"none"}
+      />
+      <TextInput
+        style={styles.input}
+        value={email}
+        placeholder={"Email"}
+        onChangeText={(text) => setEmail(text)}
+        autoCapitalize={"none"}
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        placeholder={"Password"}
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Button title={"Sign Up"} onPress={registerUser} />
     </View>
   );
 };
