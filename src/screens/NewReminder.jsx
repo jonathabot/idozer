583ef416435317620a962c351043bc9b2a7e74bf
jonathabot <lebotelhon@gmail.com>
2023-12-09 21:11:28 -0300

    new reminder editing

--- src/screens/NewReminder.jsx
+++ src/screens/NewReminder.jsx
@@ -1,7 +1,162 @@
-import { View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import {
+  BoldText,
+  LightText,
+  MediumText,
+  RegularText,
+} from "../components/Text";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 const NewReminder = () => {
-  return <View></View>;
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={styles.container}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            width: "100%",
+            marginBottom: 20,
+          }}
+        >
+          <BoldText texto="Adicionar um novo lembrete" size={16} />
+        </View>
+
+        <View
+          style={{
+            alignItems: "flex-start",
+            width: "100%",
+          }}
+        >
+          <BoldText texto="Novo lembrete" size={16} />
+          <RegularText texto="Cadastre um novo medicamento:" size={16} />
+        </View>
+
+        <View style={styles.form}>
+          <View style={{ marginTop: 20, marginBottom: 20 }}>
+            <BoldText texto="Create Account" color="#05B494" size={20} />
+          </View>
+
+          <View style={styles.formElement}>
+            <LightText texto="Username" size={13} />
+            <TextInput
+              style={styles.input}
+              value={username}
+              placeholder="Your user"
+              onChangeText={(text) => setUsername(text)}
+              autoCapitalize={"none"}
+            />
+          </View>
+
+          <View style={styles.formElement}>
+            <LightText texto="Email" size={13} />
+            <TextInput
+              style={styles.input}
+              value={email}
+              placeholder={"Your email"}
+              onChangeText={(text) => setEmail(text)}
+              autoCapitalize={"none"}
+            />
+          </View>
+
+          <View style={styles.formElement}>
+            <LightText texto="Password" size={13} />
+            <TextInput
+              style={styles.input}
+              value={password}
+              placeholder={"************"}
+              secureTextEntry
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              marginTop: 10,
+              marginBottom: 10,
+              marginLeft: -10,
+            }}
+          >
+            <TouchableOpacity>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <RadioButton
+                  value="Apple"
+                  status={terms ? "checked" : "unchecked"} //if the value of checked is Apple, then select this button
+                  onPress={() => setTerms(terms ? false : true)} //when pressed, set the value of the checked Hook to 'Apple'
+                  color="#05B494"
+                />
+                <LightText texto="I accept the terms" />
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            style={styles.buttonRegister}
+            onPress={registerUser}
+          >
+            <RegularText texto="Sign Up" color="#FFFFFF" size={16} />
+          </TouchableOpacity>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              textAlign: "center",
+              marginTop: 20,
+            }}
+          >
+            <LightText texto="Already have an account?" />
+            <TouchableOpacity onPress={navigateToLogin}>
+              <BoldTextLink texto=" Log in" color="#05B494" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 70,
+    alignItems: "center",
+    fontFamily: "light",
+    marginLeft: 30,
+    width: "85%",
+  },
+  form: {
+    width: 285,
+  },
+  input: {
+    fontFamily: "light",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingLeft: 10,
+    width: 285,
+    height: 35,
+    borderRadius: 4,
+  },
+  buttonRegister: {
+    marginTop: 10,
+    width: 285,
+    height: 36,
+    borderRadius: 4,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#05B494",
+  },
+});
 
 export default NewReminder;
